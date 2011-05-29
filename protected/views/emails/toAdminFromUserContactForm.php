e339e7b7f8d9b3f9a5038d57fdba89543f2ba59f
Ryan Kanno <ryankanno@localkinegrinds.com>
2011-05-28 14:39:13 -1000

    Removed emails views

--- protected/views/emails/toAdminFromUserContactForm.php
+++
@@ -1,9 +0,0 @@
-<h2>There was contact on the site.</h2>
-
-<p>Name: <?php echo CHtml::encode($model->name); ?></p>
-
-<p>Phone: <?php echo CHtml::encode($model->phone); ?></p>
-
-<p>Email: <?php echo CHtml::encode($model->email_address); ?></p>
-
-<p>Message: <?php echo CHtml::encode($model->message); ?></p>
