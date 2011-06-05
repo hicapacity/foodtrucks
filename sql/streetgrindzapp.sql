89150b8c82f3150da15f165db46d4ffd238326b1
Paul Lowndes <islands@gtcode.com>
2011-06-05 02:50:47 -1000

    Revised schema related to last commit for new model TwitterAccounts

--- sql/streetgrindzapp.sql
+++ sql/streetgrindzapp.sql
@@ -24,15 +24,25 @@ DROP TABLE IF EXISTS `trucks`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trucks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `twitter_id` bigint unsigned NOT NULL,
+  `twitter_id` bigint(20) unsigned NOT NULL,
   `twitter_username` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `icon_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `icon_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `twitter_id` (`twitter_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trucks`
+--
+
+LOCK TABLES `trucks` WRITE;
+/*!40000 ALTER TABLE `trucks` DISABLE KEYS */;
+INSERT INTO `trucks` VALUES (1,9491862,'austenito','http://google.com/insert_here','2011-05-29 03:48:55','2011-05-29 03:48:55'),(2,31680416,'Jason Axelson','http://google.com/insert_here','2011-06-04 22:26:45','2011-06-04 22:26:45');
+/*!40000 ALTER TABLE `trucks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `trucks_tweets`
@@ -44,9 +54,9 @@ DROP TABLE IF EXISTS `trucks_tweets`;
 CREATE TABLE `trucks_tweets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `truck_id` int(11) NOT NULL,
-  `tweet_id` bigint unsigned NOT NULL,
+  `tweet_id` bigint(20) unsigned NOT NULL,
   `tweet` varchar(160) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `photo_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `photo_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `geo_lat` float NOT NULL,
   `geo_long` float NOT NULL,
   `created` datetime NOT NULL,
@@ -56,8 +66,46 @@ CREATE TABLE `trucks_tweets` (
   KEY `truck_id_created` (`truck_id`,`created`),
   KEY `truck_id` (`truck_id`),
   CONSTRAINT `trucks_tweets_ibfk_1` FOREIGN KEY (`truck_id`) REFERENCES `trucks` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trucks_tweets`
+--
+
+LOCK TABLES `trucks_tweets` WRITE;
+/*!40000 ALTER TABLE `trucks_tweets` DISABLE KEYS */;
+INSERT INTO `trucks_tweets` VALUES (1,1,74594859209592832,'@streetgrindzapp parse me',NULL,21.292,-157.85,'2011-05-29 03:48:55','2011-05-29 03:48:55'),(2,2,77130658920673280,'@streetgrindzapp hope this has location',NULL,21.2927,-157.793,'2011-06-04 22:26:45','2011-06-04 22:26:45');
+/*!40000 ALTER TABLE `trucks_tweets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `twitter_accounts`
+--
+
+DROP TABLE IF EXISTS `twitter_accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `twitter_accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `twitter_id` bigint(20) NOT NULL,
+  `twitter_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `twitter_accounts`
+--
+
+LOCK TABLES `twitter_accounts` WRITE;
+/*!40000 ALTER TABLE `twitter_accounts` DISABLE KEYS */;
+INSERT INTO `twitter_accounts` VALUES (2,9491862,'test','2011-06-05 12:16:38','2011-06-05 12:36:01'),(3,178426199,'test2','2011-06-05 02:48:00','2011-06-05 02:48:00'),(4,31680416,'test3','2011-06-05 02:48:00','2011-06-05 02:48:00');
+/*!40000 ALTER TABLE `twitter_accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -67,4 +115,4 @@ CREATE TABLE `trucks_tweets` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-05-21 22:20:35
+-- Dump completed on 2011-06-05  2:50:43
