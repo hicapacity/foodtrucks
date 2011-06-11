e4d2796f7ac24bd5e0b8357cb44f9dd8352fd292
= <shu.chen@freelancedreams.com>
2011-06-11 12:13:09 -1000

    Initial move to jquery mobile

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,7 +15,11 @@
 	<!-- Using same css for now since we're targeting devices -->	
 	<link rel="stylesheet" media='only screen and (max-device-width: 480px)' href="<?php echo Yii::app()->request->baseUrl; ?>/css/mobile.css">
 	<link rel="stylesheet" media='only screen and (min-device-width: 480px)' href="<?php echo Yii::app()->request->baseUrl; ?>/css/mobile.css">
+
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
 	<link rel="shortcut icon" href="/images/favicon.ico" />
 
@@ -33,31 +37,26 @@
 </head>
 <body>
 <div id="page">
-	<div id="header">
-		<div id="logo"><a class="linkTitle" href="<?php echo Yii::app()->request->baseUrl;?>/"><?php echo CHtml::encode(Yii::app()->name); ?></a></div>
-		<div id="mainmenu">
-			<a href="#" id="menutoggle"><span>&laquo;</span>Menu</a>
-			<ul>
-				<li><a href="#find_all">Show All</a></li>
-				<li><a href="#find_nearest">Find Nearest</a></li>
-				<li><a href="#help">Help</a></li>
-			</ul>
-		</div><!-- mainmenu -->
-	</div><!-- header -->
-
-	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-		'links'=>$this->breadcrumbs,
-	)); ?><!-- breadcrumbs -->
-
-	<div id="content_wrapper">
-	<?php echo $content; ?>
+	<div data-fullscreen="true" data-role="page" class="content_wrapper">
+		<div data-role="header" data-position="fixed" class="header">
+			<div class="logo"><a class="linkTitle" href="<?php echo Yii::app()->request->baseUrl;?>/"><?php echo CHtml::encode(Yii::app()->name); ?></a></div>
+			<div class="mainmenu">
+				<a href="#" class="menutoggle"><span>&laquo;</span>Menu</a>
+				<ul>
+					<li><a href="#find_all">Show All</a></li>
+					<li><a href="#find_nearest">Find Nearest</a></li>
+					<li><a href="#help">Help</a></li>
+				</ul>
+			</div><!-- mainmenu -->
+		</div><!-- header -->
+		<div data-role="content" class="content">
+			<?php echo $content; ?>
+		</div>
+		<div data-role="footer" data-position="fixed" class="footer">
+			&copy; <?php echo date('Y'); ?> by <?php echo CHtml::encode(Yii::app()->params['siteName']); ?>.
+			Powered by: <a href="<?php echo CHtml::encode(Yii::app()->params['poweredByUrl']); ?>"><?php echo CHtml::encode(Yii::app()->params['poweredBy']); ?></a>
+		</div><!-- footer -->
 	</div>
-
-	<div id="footer">
-		&copy; <?php echo date('Y'); ?> by <?php echo CHtml::encode(Yii::app()->params['siteName']); ?>.
-		Powered by: <a href="<?php echo CHtml::encode(Yii::app()->params['poweredByUrl']); ?>"><?php echo CHtml::encode(Yii::app()->params['poweredBy']); ?></a>
-	</div><!-- footer -->
-
 </div><!-- page -->
 
 </body>
