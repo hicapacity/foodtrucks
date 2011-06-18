c893403858225831bb74e11c73aca6e6ef3b59b6
Analogrithems <analogrithems@gmail.com>
2011-06-17 17:46:10 -1000

    Removed info about tuck usage and sign up.  Need to discuss this with team first

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -16,15 +16,16 @@ $this->pageTitle=Yii::app()->name . ' - About';
 		<h3>What's this for?</h3>
 	   </dt>
 	   <dd>
-		<p>Well, hopefully if you are here you already have a slight understanding.  If not let me elaborate.  FoodTrucks is a simple innovative application that allows hungry people to find their favorite food fast.  In our city there are dozens of delicious food trucks all over and they move frequently.  With the FoodTrucks App you can share your current location and it will show you all the Food Trucks nearest to you.</p>
+		<p>Well, hopefully if you are here you already have a slight understanding, If not let me elaborate.  FoodTrucks is a simple innovative application that allows hungry people to find their favorite food fast.  In our city there are lots of delicious food trucks all over and they move frequently.  With the FoodTrucks App will take your current location and show you which ones are near you.</p>
 	   </dd>
+<!--
 	   <dt>
 		<h3>How do I get my truck listed here?</h3>
 	   </dt>
 	   <dd>
 		<p>This is the simple part, sign up as a Food Truck and after you are validated all you have to do is a mention to @<?php echo Yii::app()->params['trucks']['twitter']['userName']; ?> on Twitter.   That’s it, once you do that mention to twitter from your GPS enabled smart phone the FoodTrucks App will grab your location from the tweet and show it in real time on the Map for hungry people all over to find. <br/>
 
-To get Signed up as a Food Truck go <?php echo CHtml::link('here', array('site/page','view'=>'Signup'),array('id'=>'signupLink')); ?>.
+To get Signed up as a Food Truck go <?php echo CHtml::link('here', array('site/page','view'=>'signup'),array('id'=>'signupLink')); ?>.
 </p>
 	   </dd>
 	   <dt>
@@ -40,6 +41,6 @@ To get Signed up as a Food Truck go <?php echo CHtml::link('here', array('site/p
 	   </dd>
 	</dl>
 	</div>
-	
+-->
 </div>
 </div>
