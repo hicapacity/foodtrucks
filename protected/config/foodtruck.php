b671eace15112d62be728783acacfc9c7e9dcbd6
Ryan Kanno <ryankanno@localkinegrinds.com>
2011-06-25 09:55:06 -1000

    Removed unused foodtrucks config parameter

--- protected/config/foodtruck.php
+++ protected/config/foodtruck.php
@@ -7,6 +7,5 @@
             'consumerSecret'=>'qHLtP0F0z4hRfcgMM7JgGyr4lbiH3j9WJH9GwIXgAA',
             'oauthAccessToken'=>'302857632-Hd7yGOZ2kA8qCFvlWm6liSGl3H0Ibh4BZ0Ez0auO',
             'oauthAccessTokenSecret'=>'tRUtieBnPqZ3SaZsHxIzxv0FQTSoleyZtFkAm6kJ7O0',
-            'foodtrucks' => array('9491862', '178426199', '31680416'),
         ),
     );
