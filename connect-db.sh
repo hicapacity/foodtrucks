182c42b370b142a83e5e8b3dbaa03411b5d281cf
Ryan Kanno <ryankanno@localkinegrinds.com>
2011-06-25 06:21:35 -1000

    Edited connect-db to allow users to configure the helper script

--- connect-db.sh
+++ connect-db.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-mysql -u streetgrindzuser -pdev streetgrindzapp
+MYSQL_COMMAND=mysql
+MYSQL_USER=streetgrindzuser
+MYSQL_PWD=dev
+MYSQL_DB=streetgrindzapp
+
+# Allow someone to override shell script in home dir
+if [ -f $HOME/.streetgrindzconf ]; then
+    source $HOME/.streetgrindzconf
+fi
+
+# Always nice to let someone see what they are doing. :)
+echo -en "\nConnecting to MySQL with command: $MYSQL_COMMAND -u$MYSQL_USER -p$MYSQL_PWD $MYSQL_DB\n\n"
+
+$MYSQL_COMMAND -u$MYSQL_USER -p$MYSQL_PWD $MYSQL_DB
